8cbf4930f31990c4320d1c02a94cc501654f0979
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-05-31 13:13:54 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,10 @@
-i
+def fizzbuzz():
+    for x in range(1, 101):
+        if x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        elif x % 5 == 0 and x % 3 == 0:
+            print("FizzBuzz")
+         else:
+            print(x)
