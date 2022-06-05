4dd6d937b8398c99c1c95c18ebb4d69d6138f826
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-06-05 01:24:57 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()    
     for list in matrix:
         for item in list:
             print("{}".format(item), end = " ")
