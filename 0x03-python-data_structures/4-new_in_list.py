9ed3fe971956e12b95adc8e7ae9f1605798d5f7a
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-06-05 01:18:13 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
     elif idx > len(my_list) - 1:
-        return copy
+        return my_list
     else:
         for x in range(len(my_list)):
             if x == idx:
