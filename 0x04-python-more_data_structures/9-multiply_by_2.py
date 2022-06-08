53732be1c05ef3d421251e1ad584170187d751bf
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-06-08 13:21:54 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
-ii
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary
+    for key in a_dictionary.keys():
+        a_dictionary[key] = a_dictionary[key] * 2
+    return a_dictionary
