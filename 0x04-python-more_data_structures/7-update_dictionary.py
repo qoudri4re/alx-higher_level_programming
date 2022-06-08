63b26a42bcaef47dcb8ac8d770e9a5108c051d25
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-06-08 12:41:20 +0100

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,6 @@
-ii
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
