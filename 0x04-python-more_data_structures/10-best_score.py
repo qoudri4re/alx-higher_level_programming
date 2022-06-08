0f7d0dd7ec84f438bc30e1ccb0db12fb7395438c
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-06-08 13:42:03 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary and len(a_dictionary):
-        max = list(a_dictionary.keys())[0]
+    if a_dictionary:
+        max = list(a_dictionary.values())[0]
         for value in a_dictionary.values():
-        if value > max:
-            max = value
-    return max
-    else:
-        return None
+            if int(value) > int(max):
+                max = value
+        return max
+    return None
