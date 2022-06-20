6c84b021d4427e5be19fb8a8c5b052a92a4f0a9d
qoudri4re <qoudri4re@gmail.com>
2022-06-20 14:55:03 -0700

    task 4

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    res
+    try:
+        res = a / b
+    except (ValueError,  TypeError, ZeroDivisionError):
+        res = None
+    finally:
+        print("Inside result: {:d}".format(res))
