ca6ed5f181280d2ec15fe84d99d7979d0fd158fc
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-06-20 23:45:41 +0100

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
     var = 0
     for i in range(1, 3):
@@ -9,4 +10,4 @@ def magic_calculation(a, b):
         except:
             var = (b + a)
             break
-    return 
+    return var
