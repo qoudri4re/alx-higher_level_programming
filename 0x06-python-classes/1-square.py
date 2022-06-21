cf97d203a98151495d29747371f8d3175dd3e3c4
qoudri4re <qoudri4re@gmail.com>
2022-06-21 12:20:18 +0100

    completed tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,13 @@
-iiii
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a new square
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
