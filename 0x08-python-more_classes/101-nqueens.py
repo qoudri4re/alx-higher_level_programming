ea9c319a6a037a1660d08c4290eb6f33ae89dafe
qoudri4re <qoudri4re@gmail.com>
2022-06-28 12:49:23 +0100

    --

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -54,7 +54,7 @@ def Queen(m_queen, nqueen):
 
     m_queen[nqueen] = -1
 
-    while((m_queen[nqueen] < len(m_queen) - 1)):
+    while m_queen[nqueen] < len(m_queen) - 1:
 
         m_queen[nqueen] += 1
 
--- 0x08-python-more_classes/1test.py
+++ 0x08-python-more_classes/1test.py
@@ -1,5 +1,4 @@
-Rectangle = __import__('9-rectangle').Rectangle
-
-my_square = Rectangle.square(5)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
+l1 = [1, 2, 3]
+l2 = l1
+l1 = l1 + [4]
+print(l2)
