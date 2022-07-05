8e2a15dfb44b385a846cb0587aa5d8d38396ce1f
qoudri4re <qoudri4re@gmail.com>
2022-07-05 12:32:00 +0100

    completed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,12 @@
-iiii
+#!/usr/bin/python3
+
+"""reads a text file and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """reads a file and prints its contents
+    Args:
+        filename: name of the file
+    """
+    with open(filename, encoding="utf8") as myFile:
+        print(myFile.read(), end='')
