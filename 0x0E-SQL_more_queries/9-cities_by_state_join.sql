450e1e4beb2b3b467916bc4263f73282193b6836
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-08-10 13:47:48 +0100

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,6 @@
-iiii
+-- Prints and joins records from two tables with a matching field
+SELECT cities.id, cities.name, states.name
+    FROM cities
+    INNER JOIN states
+        ON cities.state_id = states.id
+    ORDER BY cities.id ASC;
