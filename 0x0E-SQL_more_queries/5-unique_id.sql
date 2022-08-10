3e4020610b10a37dbfd74e85ccf0706d8a162984
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-08-10 13:36:38 +0100

    Update 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,1 +1,7 @@
-i
+-- creates a table
+-- for each row, which is never empty when a value isn't give
+CREATE TABLE IF NOT EXISTS unique_id(
+  id INT DEFAULT 1,
+  name VARCHAR(256),
+  UNIQUE(id)
+);
