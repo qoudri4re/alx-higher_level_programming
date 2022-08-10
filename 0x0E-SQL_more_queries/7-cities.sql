6048c4741d90b0f01e7a2bdb7d71e35066f4ef5c
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-08-10 14:03:50 +0100

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- creates the database hbtn_0d_usa and 
 -- the table cities in the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
---switch database
+-- switch database
 USE htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
