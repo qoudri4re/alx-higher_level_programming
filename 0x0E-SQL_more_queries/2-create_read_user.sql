15d014995edaf7751e72731651f8eacf0f6cee41
qoudri4re <qoudri4re@gmail.com>
2022-08-10 05:26:49 -0700

    new tasks

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- creates the database  hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+
+-- creates a user with select privileges only
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
