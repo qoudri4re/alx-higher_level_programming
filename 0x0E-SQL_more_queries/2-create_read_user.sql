84680051c828cd07d14e9c09cd21948f44e25be4
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-08-10 13:56:57 +0100

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates the database  hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- creates a user with select privileges only
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
