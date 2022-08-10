b14e258618b4800d03f6ec64d589520f8b796519
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-08-10 13:40:40 +0100

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,1 +1,11 @@
-ii
+-- creates database
+CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
+-- switch the database
+USE hbtn_0d_usa;
+-- creates a table with a column that has auto increment
+-- and as a primary key
+CREATE TABLE IF NOT EXISTS states (
+  id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL,
+  UNIQUE(id)
+);
