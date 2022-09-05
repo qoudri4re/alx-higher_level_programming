ad8aafb220bf2b1b40658474f4a0198cddeaceaa
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-09-05 12:09:15 +0100

    Update 102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,1 +1,12 @@
-iii
+#!/usr/bin/node
+module.exports = {
+  /**
+   * addMeMaybe - Invokes a function after incrementing its argument by 1.
+   * @param {Number} number - The given function's argument.
+   * @param {Number} theFunction - The function to be invoked.
+   */
+  addMeMaybe: function (number, theFunction) {
+    number++;
+    theFunction(number);
+  }
+};
