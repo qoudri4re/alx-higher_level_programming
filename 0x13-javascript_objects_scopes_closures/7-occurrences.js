20313bef7efec0eeadcc075c494b30dbaa6a12ec
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-09-06 17:04:20 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 
+/**
+ * computes the number of occurrence of an element
+ * @param {Array} list The list of elements.
+ * @param {Any} searchElement The element to look for.
+ * @returns {Number} The number of occurrences of the given element.
+ */
 exports.nbOccurences = function (list, searchElement) {
   return list.filter( item => item === searchElement).length;
 };
