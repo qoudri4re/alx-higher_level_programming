20303258b272c357f3120ebc842118894c4cd456
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-09-06 16:53:31 +0100

    Update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,16 @@
-iiii
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+/**
+ * represents a square class
+ */
+class Square extends Rectangle {
+  /**
+  * instantiates objects
+  * @param {Number} size of the width and height
+  */
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
