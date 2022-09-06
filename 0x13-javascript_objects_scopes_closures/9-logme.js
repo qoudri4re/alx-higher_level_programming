b230472e4128e0566625947f59c3c877bfb79337
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-09-06 17:09:32 +0100

    Update 9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,10 @@
-ii
+#!/usr/bin/node
+let num = 0;
+/**
+ * Logs a message to the console.
+ * @param {String} item The message to be logged.
+ */
+exports.logMe = function (item) {
+  console.log(num + ': ' + item);
+  num++;
+};
