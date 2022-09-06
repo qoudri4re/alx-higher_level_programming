900f22090f641ea2af694aeea33c42a0d9c9d4f6
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-09-06 16:22:12 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,2 +1,19 @@
-iiiii`:wq
+#!/usr/bin/node
+/**
+ * represents a rectangle
+ */
+class Rectangle {
+  /**
+   * Creates a new Rectangle with the given dimensions.
+   * @param {Number} w The value of the width.
+   * @param {Number} h The value of the height.
+   */
+  constructor(w, h) {
+    if (!(w <= 0 || h <= 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
 
+module.exports = Rectangle;
