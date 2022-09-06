262a6b727966068a2ef9cacac6a98e74cc74b16b
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-09-06 17:25:32 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,13 @@
-iiii
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+
+const newDict = {};
+
+Object.getOwnPropertyNames(dict).forEach(occurences => {
+  if (newDict[dict[occurences]] === undefined) {
+    newDict[dict[occurences]] = [occurences];
+  } else {
+    newDict[dict[occurences]].push(occurences);
+  }
+});
+console.log(newDict);
