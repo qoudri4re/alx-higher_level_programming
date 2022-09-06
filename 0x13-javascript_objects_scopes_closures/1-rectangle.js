555e65eba38a1e947a8840d7d6ac5118d873fecb
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-09-06 16:13:36 +0100

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,12 @@
-iii
+#!/usr/bin/node
+/**
+ * Represents a rectangle class.
+ */
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
