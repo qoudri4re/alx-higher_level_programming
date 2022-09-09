b4c961babad98d41163ab729cd8fd8b2f386cda1
qoudri4re <qoudri4re@gmail.com>
2022-09-09 16:02:59 +0100

    --

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""A module containing the state model"""
-
+"""A module containing the State model.
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+
 Base = declarative_base()
-"""represents the base class for all tables"""
+'''Represents the base class for all tables.
+'''
 
 
 class State(Base):
-    """represents a row in a states table"""
-
+    """Represents a row in a states table.
+    """
     __tablename__ = "states"
     id = Column(
         Integer,
@@ -20,6 +22,6 @@ class State(Base):
         primary_key=True
     )
     name = Column(
-        String(lenght=128),
+        String(length=128),
         nullable=False
     )
