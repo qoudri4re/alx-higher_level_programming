c55705843fdbe1b42633ca9888d16484eac399b9
qoudri4re <qoudri4re@gmail.com>
2022-09-23 15:42:56 +0100

    hot fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """fetches a url"""
-import urllib.request as request
+from urllib import request
+
 
 if __name__ == '__main__':
     with request.urlopen("https://intranet.hbtn.io/status") as response:
