e57ed7d00c883f1282909740781e6501442e74f4
qoudri4re <qoudri4re@gmail.com>
2022-09-23 16:50:57 +0100

    --

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
-iiii
+#!/usr/bin/python3
+"""Fetches a URL."""
+import requests
+
+
+if __name__ == '__main__':
+    response = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
